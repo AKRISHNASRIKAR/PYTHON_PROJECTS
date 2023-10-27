e27a1f26c349accde7a474c605344d39befb7f1c
A KRISHNA SRIKAR <71891009+AKRISHNASRIKAR@users.noreply.github.com>
2023-10-27 23:55:35 +0530

    Update areaofcircle.py

--- areaofcircle.py
+++ areaofcircle.py
@@ -2,3 +2,15 @@ PI = 3.14
 r = float(input(' Please Enter the radius of a circle: '))
 area = PI * r * r
 print(" Area Of a Circle = %.2f" %area)
+# Accept the filename from the user
+filename = input("Enter a filename: ")
+
+# Split the filename with dot
+parts = filename.split(".")
+# There should be a dot and an extension
+if len(parts) > 1:
+    # The last element in the list is the file extension
+    extension = parts[-1]
+    print(f"The file extension is: {extension}")
+else:
+    print("This filename does not have an extension.")
